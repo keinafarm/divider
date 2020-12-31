1bc45e058e7d1e692a7269a88e0395b6b53a7885
akira <akiracraftwork@infoseek.jp>
2020-12-31 22:32:13 +0900

    カラムさがしたり

--- ExcelManager.py
+++ ExcelManager.py
@@ -22,13 +22,13 @@ def make_save_filename(file_name):
     """
     basename_without_ext = os.path.splitext(os.path.basename(file_name))[0]  # 拡張子を除いた名前
     for count in range(0, 10000):
-        basename = basename_without_ext + "_%d" % count     # "_番号"をつける
-        dir_name = os.path.dirname(file_name)               # ディレクトリパス名
-        ext_name = os.path.splitext(file_name)[1][1:]       # 拡張子
-        if ext_name != '':                                  # 拡張子があれば、間に'.'をつける
+        basename = basename_without_ext + "_%d" % count  # "_番号"をつける
+        dir_name = os.path.dirname(file_name)  # ディレクトリパス名
+        ext_name = os.path.splitext(file_name)[1][1:]  # 拡張子
+        if ext_name != '':  # 拡張子があれば、間に'.'をつける
             basename = basename + '.' + ext_name
-        filename_candidacy = os.path.join(dir_name, basename)   # パス名を再構築
-        if not os.path.exists(filename_candidacy):          # 生成したファイル名が存在しなければ、返す
+        filename_candidacy = os.path.join(dir_name, basename)  # パス名を再構築
+        if not os.path.exists(filename_candidacy):  # 生成したファイル名が存在しなければ、返す
             return filename_candidacy
 
     print("%sからファイル名を作れませんでした" % file_name)
@@ -41,10 +41,10 @@ class ExcelManager:
         指定されたExcelファイルを管理する
         :param file_name:ファイル名
         """
-        save_file_name = make_save_filename(file_name)      # 生成するファイル名
+        save_file_name = make_save_filename(file_name)  # 生成するファイル名
 
         try:
-            shutil.copy(file_name, save_file_name)          # ファイルを複写
+            shutil.copy(file_name, save_file_name)  # ファイルを複写
         except FileNotFoundError as err:
             print(type(err))
             print(err)
@@ -57,7 +57,7 @@ class ExcelManager:
             print("%sをファイル%sに複製出来ませんでした" % (file_name, save_file_name))
             sys.exit(1)
 
-        self.file_name = save_file_name                     # ファイル名確定
+        self.file_name = save_file_name  # ファイル名確定
         try:
             # Excel Bookを開く
             self.work_book = openpyxl.load_workbook(self.file_name)
@@ -82,6 +82,31 @@ class ExcelManager:
 
         self.work_book.close()
 
+    def get_sheet(self, sheet_name):
+        """
+        シートオブジェクトを得る
+        :param sheet_name:シート名
+        :return:シートオブジェクト(無い時はNone)
+        """
+        if sheet_name in self.work_book.sheetnames:
+            work_sheet = self.work_book[sheet_name]
+            return work_sheet
+        else:
+            return None
+
+    def get_column(self, sheet, column_name, column_name_line):
+        """
+        指定した行番号をラベル名の行とし、column_nameで指定された文字列をラベル名とするカラムを返す
+        :param sheet:チェックするシート
+        :param column_name:検索するラベル名
+        :param column_name_line:検索する行
+        :return:カラムオブジェクト(None=一致するものがない）
+        """
+        for cell in sheet.iter_cols(min_row=column_name_line, max_row=column_name_line):
+            if cell[0].value is not None and cell[0].value == column_name:
+                return cell[0]
+        return None
+
 
 if __name__ == "__main__":
     name = make_save_filename("C:\\develop\\python\\pythonProject\\divider\\test.data")
@@ -94,4 +119,18 @@ if __name__ == "__main__":
     print(name)
 
     obj = ExcelManager("test1.xlsx")
+
+    sheet = obj.get_sheet("test1")
+    print(sheet)
+    print(obj.get_sheet("test2"))
+
+    col = obj.get_column(sheet, "品種", 2)
+    print(col)
+    print(obj.get_column(sheet, "テスト", 2))
+
+    sheet = obj.get_sheet("圃場情報")
+    col = obj.get_column(sheet, "品種", 2)
+    print(col)
+    print(obj.get_column(sheet, "テスト", 2))
+
     obj.close()
---
+++ divider.py
@@ -0,0 +1,61 @@
+##########################################################
+#
+#   指定したファイルの指定したシートの指定したカラムの内容と同じ名前でシートを作って
+#   そこに、コピーする
+#
+#   Usage:
+#       > divider  file名 シート名 カラム名 [タイトル行数]
+#           file名 : 抽出するExcelファイル名
+#           シート名 : 抽出するデータが入っているシート
+#           カラム名 : 抽出するキーワードとなるカラム名
+#           タイトル行数 : タイトルとして各シートにコピーする行数（省略時は2)
+#
+#   カラム名で指定されたカラムのタイトル行数以降のセルに入っている文字列を抽出し
+#   ソートしてから、重複を省く
+#   上記の処理で出来たリストを、シート名として新たにシートを作成する
+#   各シートには、それぞれカラム名で指定されたカラムの内容が、シート名と一致する行を
+#   シート名で指定されたシートからコピーする
+#   但し、各シートの上から「タイトル行数」分には、シート名で指定されたシートから無条件にコピーされる
+#
+##########################################################
+
+from ExcelManager import ExcelManager
+import sys
+
+class Divider:
+    def __init__(self, file_name, sheet_name, column_name, title_lines):
+        """
+
+        :param file_name:
+        :param sheet_name:
+        :param column_name:
+        :param title_lines:
+        """
+        self.work_book = ExcelManager(file_name)
+        self.sheet_name = sheet_name
+        self.column_name = column_name
+        self.title_lines = title_lines
+
+        source_sheet = self.work_book.get_sheet(self.sheet_name)
+        if source_sheet is None:
+            print("元になるシート%sがありません"%sheet_name)
+            sys.exit(1)
+
+        columns = self.work_book.get_column(source_sheet, column_name)
+        if columns is None:
+            print("元になるカラム%sがありません"%column_name)
+            sys.exit(1)
+
+if __name__ == "__main__":
+    parameters = len(sys.args)
+    if len < 4 or len > 5:
+        print("Usage:")
+        print("   divider  file名 シート名 カラム名 [タイトル行数]")
+        sys.exit(1)
+
+    if len == 4:
+        lines = 2
+    else:
+        lines = int(sys.args[4])
+
+    obj = Divider(sys.args[1], sys.args[2], sys.args[3], lines)
